f650904662444131871f43de76b3134cd5bc2111
Kings Dev <97048206+Nguepi-Christian@users.noreply.github.com>
2023-08-26 03:37:28 +0100

    Update MongoDb.js

--- Api/MongoDb.js
+++ Api/MongoDb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mongoConnection = () =>{
      mongoose.connect("mongodb+srv://gopepeter:azerty@free.rtxn2k4.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true},()=>{
-     console.log("Connected to mongoDB ");
+     console.log("Connected to mongoDB !");
     })
 };
 
